bce6880dae6a941e9051e18d7f15a3a93d496ab2
sandeep reddy <sandeepreddyr12@gmail.com>
2023-07-17 22:10:21 +0530

    added navbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function Navbar() {
   return (
-    <nav className="w-full bg-black fixed top-0 left-0 right-0 z-10">
+    <nav className="w-full  fixed top-0 left-0 right-0 z-10">
       <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div className="flex items-center justify-between py-3 md:py-5 md:block">
           {/* LOGO */}
@@ -31,8 +31,8 @@ function Navbar() {
         <div
           className={`
            justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-            true ? 'p-12 md:p-0 block' : 'hidden'
-          }`}
+             true ? 'p-12 md:p-0 block' : 'hidden'
+           }`}
         >
           <ul className="h-screen md:h-auto items-center justify-center md:flex">
             <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
