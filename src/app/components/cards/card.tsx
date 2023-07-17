c405b03c1d3a27669925a3303bf53715b646a521
sandeep reddy <sandeepreddyr12@gmail.com>
2023-07-17 22:08:51 +0530

    added contest cards component{

--- src/app/components/cards/card.tsx
+++ src/app/components/cards/card.tsx
@@ -1,45 +1,63 @@
 import Image from 'next/image';
 
-export default function Card() {
+import { contestProps } from '../../Types/types';
+import Timer from '../timeCounter/timerlib';
+
+interface contestCardProps {
+  data: contestProps;
+}
+
+export default function Card({ data }: contestCardProps) {
+  // console.log(data);
+
+  const { teamA, teamB, status, result, matchId } = data;
+
   return (
-    <div className="max-w-md min-w-max w-1/5 mx-3 my-4 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+    <div className="w-[22rem] relative mx-3 my-4 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+       <div className="h-5 w-max px-3 bg-green-500 rounded-full absolute top-1.5 right-3 hover:opacity-80 cursor-pointer ">
+        <div className="relative">
+          <p className="text-xs pt-0.5 text-blue-100 font-semibold text-center align-text-bottom">
+            Live
+          </p>
+        </div>
+        </div>
       <div className="w-full  bg-white rounded-lg p-4 dark:bg-gray-800">
         <div className="text-cyan-200 text-center "> icc odi worldcup</div>
-        <dl className="grid max-w-screen-xl grid-cols-2 gap-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white">
-          <div className=" py-3 px-6 flex flex-col items-center justify-center">
+        <dl className="grid grid-cols-2 gap-4 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
+          <div className=" py-2 px-4 flex flex-col items-center justify-center">
             <div>
               <Image
-                width="48"
-                height="48"
+                width="30"
+                height="30"
                 src="https://img.icons8.com/color/96/india.png"
                 alt="india"
               />
             </div>
             <dd className="text-gray-500 text-xs dark:text-gray-400">
-              country
+              {teamA}
             </dd>
           </div>
-          <div className=" py-3 px-6 flex flex-col items-center justify-center">
+          <div className=" py-2 px-4 flex flex-col items-center justify-center">
             <dt className="mb-2  text-xs">Today</dt>
             <dd className="text-gray-500  text-sm font-bold dark:text-gray-400">
-              time
+              <Timer />
             </dd>
           </div>
-          <div className=" py-3 px-6 flex flex-col items-center justify-center">
+          <div className=" py-2 px-4 flex flex-col items-center justify-center">
             <dt className="mb-2 text-sm font-bold">flag</dt>
             <dd className="text-gray-500 text-xs dark:text-gray-400">
-              country
+              {teamB}
             </dd>
           </div>
-          <div className="bg-gray-600 py-3 px-6 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <div className="bg-gray-600 py-2 px-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
             <dt className="mb-2 text-sm font-bold">1.5</dt>
             <dd className="text-gray-500 text-xs dark:text-gray-400">1</dd>
           </div>
-          <div className="bg-gray-600 py-3 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <div className="bg-gray-600 py-2 px-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
             <dt className="mb-2 text-sm font-bold">90+</dt>
             <dd className="text-gray-500 text-xs dark:text-gray-400">X</dd>
           </div>
-          <div className="bg-gray-600 py-3 shadow-lg rounded-lg flex flex-col items-center justify-center">
+          <div className="bg-gray-600 py-2 px-4 shadow-lg rounded-lg flex flex-col items-center justify-center">
             <dt className="mb-2 text-sm font-bold">1.2</dt>
             <dd className="text-gray-500 text-xs dark:text-gray-400">2</dd>
           </div>
--- src/app/components/cards/cards.tsx
+++ src/app/components/cards/cards.tsx
@@ -1,12 +1,43 @@
+'use client';
+
 import { JsxElement } from 'typescript';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { collection, doc } from 'firebase/firestore';
+import Countdown from 'react-countdown';
+
 import Card from './card';
+import { db } from '../../../../firebase';
 export default function cards() {
-  let arr = [1, 2, 3, 4, 5, 6, 7];
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [contests, loading, error] = useCollection(
+    collection(db, 'moralis', 'events', 'Contests')
+  );
+
+  // console.log(contests?.docs);
+
   return (
-    <div className='w-full justify-center flex flex-wrap items-center'>
-      {arr.map((a, i) => (
-        <Card key={a} />
-      ))}
+    <div>
+      {error && <strong>Error: {JSON.stringify(error)}</strong>}
+      {loading && <span>Collection: Loading...</span>}
+      {contests && (
+        <div className="w-full justify-center flex flex-wrap items-center">
+          {contests.docs.map((doc) => {
+            //  console.log(doc.data());
+
+            let docData = doc.data();
+
+            let contest = {
+              teamA: docData.teamA,
+              teamB: docData.teamB,
+              status: docData.status,
+              result: docData.contestResult,
+              matchId: docData.matchId,
+            };
+
+            return <Card key={doc.id} data={contest} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
