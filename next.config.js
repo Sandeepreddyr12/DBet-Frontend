a1f513dbcdeb47fbb703be6d54069183a0b6ff81
sandeep reddy <sandeepreddyr12@gmail.com>
2023-08-23 22:33:42 +0530

    changes in config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['img.icons8.com'],
   },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 module.exports = nextConfig
