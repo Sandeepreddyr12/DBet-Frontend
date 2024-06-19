862fcfee7c5b2d6faf10c4229b8180cfa944e3ab
sandeep reddy <sandeepreddyr12@gmail.com>
2024-06-19 21:58:47 +0530

    minor ui/svg bug corrections

--- src/app/components/myMatches/profile/Profile.tsx
+++ src/app/components/myMatches/profile/Profile.tsx
@@ -40,7 +40,7 @@ export default function Profile({}: Props) {
                 <path
                   fillRule="evenodd"
                   d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>{' '}
             </div>{' '}
